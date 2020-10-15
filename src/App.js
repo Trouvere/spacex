e117f46fdf8f9c7e1684ce7a1d6b3e8cbff83c42
Mishyn Aliaksei <trouvere777@mail.ru>
2020-10-15 18:26:05 +0300

    Add calendar, details, features, footer,

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React from "react";
 import "./css/style.css";
 import Header from "./components/header";
 import Main from "./components/main";
+import Features from "./components/features";
+import Footer from "./components/footer";
+
+import Calendar from "./components/calendar";
+import Details from "./components/details";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Features />
+      {/* <Details /> */}
+      <Footer />
     </div>
   );
 }
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -4,11 +4,17 @@ import "./main.css";
 
 const Main = () => {
   return (
-    <section class="main">
-      <h1 class="title">Falcon 1</h1>
+    <section className="main">
+      <h1 className="title">Falcon 1</h1>
 
-      <div class="video-container">
-        <video class="video" autoPlay loop muted src="./video/moon.mp4"></video>
+      <div className="video-container">
+        <video
+          className="video"
+          autoPlay
+          loop
+          muted
+          src="./video/moon.mp4"
+        ></video>
       </div>
     </section>
   );
