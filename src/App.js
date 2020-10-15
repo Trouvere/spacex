4a899d0d31a6cbdd00715822cf7ea211eab723c6
Mishyn Aliaksei <trouvere777@mail.ru>
2020-10-15 12:26:55 +0300

    Create and clean project, add header, main, img...

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import logo from "./img/logo.svg";
+import "./css/style.css";
+import Header from "./components/header";
+import Main from "./components/main";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
